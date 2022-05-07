34bd7179c2f9205ebaeefa4beb9cb8961bdf4075
Nicholas George <nicholas.george32@gmail.com>
2022-05-07 17:38:45 -0400

    updates to api call and organizing metadata

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 // code to handle queries
+const searchQuery = document.getElementById("itemsearch");
+const searchConceptDiv = document.getElementById("searchConceptDiv");
+const chooser = document.getElementById("chooser");
+const polite = "&mailto=nicholas.george32@gmail.com";
+
+
 function openAlexSearch(term){
     // search concepts via openalex
-    let api_call = "http://api.openalex.org/autocomplete/concepts?q="+term;
+    let api_call = "http://api.openalex.org/autocomplete/concepts?q=" + term + polite;
     jsonres = fetch(api_call,{method: 'GET'})
         .then(function(response){return response.json();});
     return jsonres;
@@ -10,15 +16,17 @@ function openAlexSearch(term){
 
 function updateInfoDiv(data){
     console.log("Updating div");
-    let chooser = document.getElementById("chooser");
     if (!data){
         console.log("No data, clearing");
         // remove all items
         chooser.innerHTML="";
         chooser.style.visibility = "hidden";
+        searchConceptDiv.innerHTML = "";
+        searchConceptDiv.style.visibility = "hidden";
         return;
     }
     chooser.style.visibility= "visible";
+    searchConceptDiv.style.visibility = "visible"
     // remove all items
     chooser.innerHTML="";
     for (let i = 0; i < data.length; i++){
@@ -29,20 +37,40 @@ function updateInfoDiv(data){
         chooser.appendChild(current);
     }
     chooser.selectedIndex = 0;
-    // call other functions here
-    // attach listener to the new selection
-    // make selection
+    searchConcept();
     return;
 }
 
+function searchConcept(){
+    console.log("Current selection is " + chooser.value);
+    conceptRes = fetch(chooser.value + polite, {method: 'GET'})
+        .then((res) => {return res.json()})
+        .then((resjson) => {updateSearchConceptDiv(resjson)});
+}
+
+function updateSearchConceptDiv(response){
+    searchConceptDiv.innerHTML = "";
+    console.log(response.description);
+    let heading = document.createElement("h2");
+    heading.innerHTML = response.display_name;
+    let description = document.createElement("p");
+    description.innerHTML = response.description;
+    let plot = document.createElement("div");
+    plot.id = "plotlyCitations";
+    plot.innerHTML = "test";
+    searchConceptDiv.append(heading, description, plot);
+}
 
 
-// setup action listeners
-const searchQuery = document.querySelector("#itemsearch");
+// setup event listeners and clear fields
+
+searchQuery.value = "";
+
 searchQuery.addEventListener("keyup", function (evt) {
-    var searchres = openAlexSearch(searchQuery.value)
-        //.then((message) => {message.map(parseResponse)});
-    //searches.map(parseResponse);
+    openAlexSearch(searchQuery.value)
         .then((message)=> {updateInfoDiv(message.results)});    
 });
 
+chooser.addEventListener("change", function(evt){
+    searchConcept()
+});
